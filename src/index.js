379e4bc7c1d7abbe4bca3f9588b069564efc2f3b
Андрей Поташин <PotashinAndrey@users.noreply.github.com>
2021-03-13 20:15:00 +0500

    task resolved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 module.exports = function toReadable (number) {
-  
+    if (number === 0) return 'zero';
+
+    let result = '';
+
+    const units = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const dozen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const dozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    const numbers = `${number}`.split('');
+
+    console.log(numbers);
+
+    if (numbers.length === 3) {
+        result += `${units[+numbers[0] - 1]} hundred`;
+
+        if (numbers[1] !== '0') {
+            if (numbers[1] === '1') {
+                result += ` ${dozen[+numbers[2]]}`;
+
+                return result;
+            }
+
+            result += ` ${dozens[+numbers[1] - 2]}`;
+        }
+
+        if (numbers[2] !== '0') {
+            result += ` ${units[+numbers[2] - 1]}`;
+        }
+    }
+
+    if (numbers.length === 2) {
+        if (numbers[0] !== '0') {
+            if (numbers[0] === '1') {
+                result += `${dozen[+numbers[1]]}`;
+
+                return result;
+            }
+
+            result += `${dozens[+numbers[0] - 2]}`;
+        }
+
+        if (numbers[1] !== '0') {
+            result += ` ${units[+numbers[1] - 1]}`;
+        }
+    }
+
+    if (numbers.length === 1) {
+        return `${units[+numbers[0] - 1]}`
+    }
+
+
+    return result;
 }
